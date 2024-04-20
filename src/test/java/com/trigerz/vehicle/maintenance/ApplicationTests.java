5284123bc75412ee7d7a40a6c70774e031d9e20d
TriGerZ <franck.mazzolo@gmail.com>
2024-04-20 15:04:58 +0200

    Init project

---
+++ src/test/java/com/trigerz/vehicle/maintenance/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trigerz.vehicle.maintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
